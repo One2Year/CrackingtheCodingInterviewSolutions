82c1450f1a48713f1ef2c14c210a7f71deaa22fb
小米-刘相廷 <793150526@qq.com>
2021-06-07 00:10:51 +0800

    Create OneAway.java

---
+++ Arrays and Strings/Q1_05_One_Away/OneAway.java
@@ -0,0 +1,26 @@
+/**
+ * There are three types of edits that can be performed on strings: insert a character, 
+ * remove a character, or replace a character. Given two strings, write a function to check 
+ * if they are one edit (or zero edits) away.
+ * 
+ * EXAMPLE
+ * pale, ple -> true 
+ * pales, pale -> true 
+ * pale, bale -> true 
+ * pale, bake -> false
+*/
+
+public class OneAway {
+    /**
+     * pale
+     * ple
+     * 
+     * 1.insert
+     *  length gap <= 1
+     * 2.remove
+     *  
+     * 3.replace
+    */
+    
+    
+}
